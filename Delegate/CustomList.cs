064222c67a9d1c0f53e428955c5e64f1b352453e
RizvanVeliyev <veliyevrizvan250@gmail.com>
2024-07-17 17:29:42 +0400

    Add project files.

---
+++ Delegate/CustomList.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    public class CustomList<T>
+    {
+        private T[] _items;
+        private int _size;
+        private int _capacity;
+
+        public CustomList()
+        {
+            _capacity = 4;
+            _items = new T[_capacity];
+            _size = 0;
+        }
+
+        public void Add(T item)
+        {
+            if (_size == _capacity)
+            {
+                _capacity *= 2;
+                T[] newItems = new T[_capacity];
+                for (int i = 0; i < _size; i++)
+                {
+                    newItems[i] = _items[i];
+                }
+                _items = newItems;
+            }
+            _items[_size] = item;
+            _size++;
+        }
+
+        public T Find(T item)
+        {
+            for (int i = 0; i < _size; i++)
+            {
+                if (_items[i].Equals(item))
+                {
+                    return _items[i];
+                }
+            }
+            return default(T);
+        }
+
+        public List<T> FindAll(T item)
+        {
+            List<T> result = new List<T>();
+            for (int i = 0; i < _size; i++)
+            {
+                if (_items[i].Equals(item))
+                {
+                    result.Add(_items[i]);
+                }
+            }
+            return result;
+        }
+
+        public void Remove(T item)
+        {
+            for (int i = 0; i < _size; i++)
+            {
+                if (_items[i].Equals(item))
+                {
+                    for (int j = i; j < _size - 1; j++)
+                    {
+                        _items[j] = _items[j + 1];
+                    }
+                    Array.Resize(ref _items, --_size);
+                    return;
+                }
+            }
+            Console.WriteLine("bele bir element yoxdur!");
+        }
+
+        public void RemoveAll(T item)
+        {
+            bool isRemoved = false;
+            for (int i = 0; i < _size; i++)
+            {
+                if (_items[i].Equals(item))
+                {
+                    isRemoved = true;
+                    Remove(_items[i]);
+                }
+            }
+            if (!isRemoved) Console.WriteLine("bele element yoxdur!");
+        }
+
+        public int Count => _size;
+        public T this[int index]
+        {
+            get=> _items[index];
+            set
+            {
+                _items[index] = value;
+            }
+            
+        }
+
+    }
+}
+
---
+++ Delegate/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Delegate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomList<int> list = new CustomList<int>();
+
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            list.Add(15);
+            list.Add(2);
+            list.Add(34);
+            list.Add(2);
+
+            Console.WriteLine("Our list:");
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.Write(list[i]+" ");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+            int found = list.Find(3);
+
+            Console.WriteLine($"Found: {found}");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            List<int> foundAll = list.FindAll(2);
+            Console.WriteLine("Find all 2:");
+            foreach (int num in foundAll)
+            {
+                Console.Write(num+" ");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+            list.Remove(3);
+            Console.WriteLine("List after removing element 3:");
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.Write(list[i]+ " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            list.RemoveAll(2);
+            Console.WriteLine("List after remove all 2:");
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.Write(list[i]+" ");
+            }
+        }
+    }
+}
